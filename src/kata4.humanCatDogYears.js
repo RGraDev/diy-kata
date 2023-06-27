20f7ca31fe629ba750ae9e22eb215130e5994d71
Ryan Granger <ryan.granger98@gmail.com>
2023-06-27 21:27:23 +0100

    tests and code written for humanCatDogYears

--- src/kata4.humanCatDogYears.js
+++ src/kata4.humanCatDogYears.js
@@ -1,3 +1,26 @@
-const humanCatDogYears = (number) => {};
+const humanCatDogYears = (number) => {
+  let counter = 1;
+  let catYears = 0;
+  let dogYears = 0;
+
+  while (counter <= number) {
+    if (counter === 1) {
+      catYears += 15;
+      dogYears += 15;
+      counter += 1;
+    } else if (counter === 2) {
+      catYears += 9;
+      dogYears += 9;
+      counter += 1;
+    } else {
+      catYears += 4;
+      dogYears += 5;
+      counter += 1;
+    }
+  }
+
+  const array = [number, catYears, dogYears];
+  return array;
+};
 
 module.exports = humanCatDogYears;
--- test/kata4.humanCatDogYears.test.js
+++ test/kata4.humanCatDogYears.test.js
@@ -1,3 +1,11 @@
 const { humanCatDogYears } = require("../src");
 
-// Look Ma, no handlebars!!!
+describe("humanCatDogYears", () => {
+  test("Returns array of [human years, cat years, dog years]", () => {
+    expect(humanCatDogYears(10)).toEqual([10, 56, 64]);
+    expect(humanCatDogYears(30)).toEqual([30, 136, 164]);
+    expect(humanCatDogYears(1)).toEqual([1, 15, 15]);
+    expect(humanCatDogYears(2)).toEqual([2, 24, 24]);
+    expect(humanCatDogYears(0)).toEqual([0, 0, 0]);
+  });
+});

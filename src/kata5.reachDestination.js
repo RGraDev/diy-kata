df06088b387791600b8aa9681e18b5336b89b0fd
Ryan Granger <ryan.granger98@gmail.com>
2023-06-28 13:54:46 +0100

    tests and code written for reachDestination

--- src/kata5.reachDestination.js
+++ src/kata5.reachDestination.js
@@ -1,3 +1,16 @@
-const reachDestination = (distance, speed) => {};
+function reachDestination(distance, speed) {
+  const estimatedArrival = distance / speed;
+
+  if (distance < 0 || speed < 0) {
+    return "Distance and Speed must both be positive";
+  }
+  if (speed === 0) {
+    return "Speed cannot be zero";
+  }
+  if (distance === 0) {
+    return "Distance cannot be zero";
+  }
+  return `I should be there in ${Math.round(estimatedArrival * 2) / 2} hours`;
+}
 
 module.exports = reachDestination;
--- test/kata5.reachDestination.test.js
+++ test/kata5.reachDestination.test.js
@@ -1,5 +1,24 @@
 const { reachDestination } = require("../src");
 
 describe("reachDestination", () => {
-  test("returns string with estimated time of arrival", () => {});
+  test("returns string with estimated time of arrival", () => {
+    expect(reachDestination(30, 23)).toBe("I should be there in 1.5 hours");
+    expect(reachDestination(125, 42)).toBe("I should be there in 3 hours");
+    expect(reachDestination(7, 3)).toBe("I should be there in 2.5 hours");
+  });
+  test("returns error message if user inputs negative distance or speed", () => {
+    expect(reachDestination(-20, 30)).toBe(
+      "Distance and Speed must both be positive"
+    );
+    expect(reachDestination(20, -30)).toBe(
+      "Distance and Speed must both be positive"
+    );
+    expect(reachDestination(-10, -20)).toBe(
+      "Distance and Speed must both be positive"
+    );
+  });
+  test("returns message if user inputs value of zero for distance or speed", () => {
+    expect(reachDestination(0, 30)).toBe("Distance cannot be zero");
+    expect(reachDestination(20, 0)).toBe("Speed cannot be zero");
+  });
 });

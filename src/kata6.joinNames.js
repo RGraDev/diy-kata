51e48e87d81063654e95af656caced4767f99766
Ryan Granger <ryan.granger98@gmail.com>
2023-06-29 10:21:20 +0100

    tests and code written for joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,15 @@
-const joinNames = (namesObj) => {};
+const joinNames = (namesObj) => {
+  let resultString = "";
+  for (let i = 0; i < namesObj.length; i += 1) {
+    if (i === namesObj.length - 1) {
+      resultString += `${namesObj[i].name}`;
+    } else if (i === namesObj.length - 2) {
+      resultString += `${namesObj[i].name} & `;
+    } else {
+      resultString += `${namesObj[i].name}, `;
+    }
+  }
+  return resultString;
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,10 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  test("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{ name: "Bart" }, { name: "Maggie" }, { name: "Lisa" }])).toBe("Bart, Maggie & Lisa");
+    expect(joinNames([{ name: "Bart" }, { name: "Maggie" }])).toBe("Bart & Maggie");
+    expect(joinNames([{ name: "Bart" }])).toBe("Bart");
+    expect(joinNames([{ name: "Bart" }, { name: "Maggie" }, { name: "Lisa" }, {name: "Homer"}])).toBe("Bart, Maggie, Lisa & Homer");
+  });
 });

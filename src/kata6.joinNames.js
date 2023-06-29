dfab22ff21f17c4a73f37070c6f2cefadd60a05b
Ryan Granger <ryan.granger98@gmail.com>
2023-06-29 10:27:32 +0100

    Refactored joinNames

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,6 +1,6 @@
 const joinNames = (namesObj) => {
   let resultString = "";
-  for (let i = 0; i < namesObj.length; i += 1) {
+  for (let i = 0; i < namesObj.length; i++) {
     if (i === namesObj.length - 1) {
       resultString += `${namesObj[i].name}`;
     } else if (i === namesObj.length - 2) {
